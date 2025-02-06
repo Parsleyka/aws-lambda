a5a229cb4b346acdcd7c7fc1f80dc30573193ff7
Naazarii Hryhorash <grigorashnazar@gmail.com>
2025-02-06 14:12:20 +0200

    Task5

--- task05/app/lambdas/api_handler/index.js
+++ task05/app/lambdas/api_handler/index.js
@@ -1,35 +1,44 @@
 const AWS = require('aws-sdk');
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-const tableName = 'cmtr-72e50b99-Events';
+const tableName = 'Events';
 
 exports.handler = async (event) => {
+    console.log('VARIABLES: ' + JSON.stringify(process.env));
     console.log('Event: ' + JSON.stringify(event));
 
     const requestContext = event.requestContext;
 
-    if(requestContext.resourcePath === '/events'){
-        switch (requestContext.httpMethod) {
-            case 'POST':
-                const body = JSON.parse(event.body);
+    try {
+        if(requestContext.resourcePath === '/events'){
+            switch (requestContext.httpMethod) {
+                case 'POST':
+                    const body = JSON.parse(event.body);
 
-                const params = {
-                    TableName: tableName,
-                    Item: body
-                }
+                    console.log('POST Body: ' + JSON.stringify(body));
 
-                const res = await dynamoDb.put(params).promise();
+                    const params = {
+                        TableName: tableName,
+                        Item: body
+                    }
 
-                return {
-                    statusCode: 201,
-                    body: JSON.stringify(res),
-                };
-            default:
-                return {
-                    statusCode: 404,
-                    body: JSON.stringify('Not Found'),
-                };
+                    console.log('Event: ' + JSON.stringify(params));
+
+                    const res = await dynamoDb.put(params).promise();
+
+                    return {
+                        statusCode: 201,
+                        body: JSON.stringify(res),
+                    };
+                default:
+                    return {
+                        statusCode: 404,
+                        body: JSON.stringify('Not Found'),
+                    };
+            }
         }
+    } catch (e) {
+        console.log('Try Catch Error: ' + JSON.stringify(e));
     }
 
     return {
