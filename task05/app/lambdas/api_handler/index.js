b8d785dcca9aeced055534e68394b8dbe2f5277f
Naazarii Hryhorash <grigorashnazar@gmail.com>
2025-02-06 22:10:29 +0200

    Task5

--- task05/app/lambdas/api_handler/index.js
+++ task05/app/lambdas/api_handler/index.js
@@ -1,31 +1,56 @@
-const { v4: uuidv4 } = require('uuid');
-const AWS = require("aws-sdk");
-const docClient = new AWS.DynamoDB.DocumentClient();
-const tableName = process.env.target_table || "Events"
+const AWS = require('aws-sdk');
+const uuid = require('uuid');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+const tableName = process.env.target_table || "Events";
 
 exports.handler = async (event) => {
-    console.log(event)
-    const params = {
-        TableName: tableName,
-        Item: {
-            "id": uuidv4(),
-            "principalId": event["principalId"],
-            "createdAt": new Date().toISOString(),
-            "body": event["content"]
-        }
-    };
+    console.log('Variables: ' + JSON.stringify(process.env));
+    console.log('Event: ' + JSON.stringify(event));
+
+    const requestContext = event.requestContext;
+
     try {
-        const data = await docClient.put(params).promise();
-        console.log("Added item:", JSON.stringify(data, null, 2));
-        return {
-            "statusCode": 201,
-            "event": params['Item']
-        };
-    } catch (err) {
-        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-        return {
-            "statusCode": 500,
-            "errorMessage": JSON.stringify(err, null, 2)
-        };
+        if(requestContext.resourcePath === '/events'){
+            switch (requestContext.httpMethod) {
+                case 'POST':
+                    const body = JSON.parse(event.body);
+
+                    console.log('/events POST body: ' + JSON.stringify(body));
+
+                    const params = {
+                        TableName: tableName,
+                        Item: {
+                            id: uuid.v4(),
+                            principalId: body.principalId,
+                            createdAt: new Date().toISOString(),
+                            body: body.content
+                        }
+                    }
+
+                    console.log('dynamoDb params: ' + JSON.stringify(params));
+
+                    await dynamoDb.put(params).promise();
+
+                    console.log('/events POST success');
+
+                    return {
+                        statusCode: 201,
+                        body: params.Item
+                    };
+                default:
+                    return {
+                        statusCode: 404,
+                        body: 'Not Found',
+                    };
+            }
+        }
+    } catch (e) {
+        console.log('Try Catch Error: ' + JSON.stringify(e));
+    }
+
+    return {
+        statusCode: 404,
+        body: 'Not Found',
     }
 };
